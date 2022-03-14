b786881b373fa1547936a7bc21b332a49bd0aeba
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-03-14 13:20:56 +0300

    removes empty module

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // text
 
 import './map/map-popup.js'; // всплывающая карточка на карте
-import './server.js'; // взаимодействие с сервером
 
 // createOfferList();
 // console.log(createOfferList());
